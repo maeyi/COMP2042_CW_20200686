8df8ac2bbf61bd376318c487b759ca30ec365691
maeyi <junmaemae@icloud.com>
2021-12-03 15:37:09 +0800

    Update HomeMenuView.java

--- src/main/java/view/HomeMenuView.java
+++ src/main/java/view/HomeMenuView.java
@@ -39,6 +39,7 @@ public class HomeMenuView extends JComponent implements MouseListener, MouseMoti
     private static final String CREDITS = "Version 0.1";
     private static final String START_TEXT = "Start";
     private static final String MENU_TEXT = "Exit";
+    private static final String INFO_TEXT = "Info";
 
     private static final Color BG_COLOR = Color.YELLOW.brighter();//change background collor to yellow
     private static final Color BORDER_COLOR = new Color(200,8,21); //Venetian Red
@@ -52,6 +53,7 @@ public class HomeMenuView extends JComponent implements MouseListener, MouseMoti
     private Rectangle menuFace;
     private Rectangle startButton;
     private Rectangle menuButton;
+    private Rectangle infoButton;
 
 
     private BasicStroke borderStoke;
@@ -66,6 +68,7 @@ public class HomeMenuView extends JComponent implements MouseListener, MouseMoti
 
     private boolean startClicked;
     private boolean menuClicked;
+    private boolean infoClicked;
 
 
     /**
@@ -95,6 +98,7 @@ public class HomeMenuView extends JComponent implements MouseListener, MouseMoti
         Dimension btnDim = new Dimension(area.width / 3, area.height / 12);
         startButton = new Rectangle(btnDim);
         menuButton = new Rectangle(btnDim);
+        infoButton = new Rectangle(btnDim);
 
         borderStoke = new BasicStroke(BORDER_SIZE,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND,0,DASHES,0);
         borderStoke_noDashes = new BasicStroke(BORDER_SIZE,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND);
@@ -232,11 +236,12 @@ public class HomeMenuView extends JComponent implements MouseListener, MouseMoti
 
         Rectangle2D txtRect = buttonFont.getStringBounds(START_TEXT,frc);
         Rectangle2D mTxtRect = buttonFont.getStringBounds(MENU_TEXT,frc);
+        Rectangle2D iTxtRect = buttonFont.getStringBounds(INFO_TEXT,frc);
 
         g2d.setFont(buttonFont);
 
         int x = (menuFace.width - startButton.width) / 2;
-        int y =(int) ((menuFace.height - startButton.height) * 0.8);
+        int y =(int) ((menuFace.height - startButton.height) * 0.65);
 
         startButton.setLocation(x,y);
 
@@ -291,6 +296,36 @@ public class HomeMenuView extends JComponent implements MouseListener, MouseMoti
             g2d.draw(menuButton);
             g2d.drawString(MENU_TEXT,x,y);
         }
+        //adding info button
+        x = startButton.x;
+        y = startButton.y;
+
+        y *= 1.4;
+
+        infoButton.setLocation(x,y);
+
+
+        x = (int)(infoButton.getWidth() - iTxtRect.getWidth()) / 2;
+        y = (int)(infoButton.getHeight() - iTxtRect.getHeight() / 2);
+
+
+
+        x += infoButton.x;
+        y += infoButton.y + (menuButton.height * 0.5);
+
+        if(infoClicked){
+            Color tmp = g2d.getColor();
+
+            g2d.setColor(CLICKED_BUTTON_COLOR);
+            g2d.draw(infoButton);
+            g2d.setColor(CLICKED_TEXT);
+            g2d.drawString(INFO_TEXT,x,y);
+            g2d.setColor(tmp);
+        }
+        else{
+            g2d.draw(infoButton);
+            g2d.drawString(INFO_TEXT,x,y);
+        }
 
     }
 
@@ -329,6 +364,11 @@ public class HomeMenuView extends JComponent implements MouseListener, MouseMoti
         else if(menuButton.contains(p)){
             menuClicked = true;
             repaint(menuButton.x,menuButton.y,menuButton.width+1,menuButton.height+1);
+        }
+        else if (infoButton.contains(p))
+        {
+            infoClicked = true;
+            repaint(infoButton.x,infoButton.y,infoButton.width+1,infoButton.height+1);
         }
     }
 
@@ -372,7 +412,7 @@ public class HomeMenuView extends JComponent implements MouseListener, MouseMoti
     @Override
     public void mouseMoved(MouseEvent mouseEvent) {
         Point p = mouseEvent.getPoint();
-        if(startButton.contains(p) || menuButton.contains(p))
+        if(startButton.contains(p) || menuButton.contains(p) || infoButton.contains(p))
             this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         else
             this.setCursor(Cursor.getDefaultCursor());
